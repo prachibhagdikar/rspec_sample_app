cc2d8ad3fd13d498ba33bbd0d824b8c18edae0c0
Prachi Bhagdikar <prachi.bhagdikar@cuelogic.com>
2021-05-06 18:30:23 +0530

    added products index rspec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 RSpec.describe ProductsController, type: :controller do
 	
   describe "index" do  	
-  	let(:product) {create :product }
+  	let(:admin) {create :admim }
+  	before do
+      controller.stub(:current_user){ admin }
+    end
     it "admin user Should be able see products with new, edit lik of product" do
-    	
+	    get :index
+	    expect(response).to render_template("index")
     end
   end
 end
